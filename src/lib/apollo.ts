963ca6ded289a47d35fd3143f8240650cb57cc86
Rafael Berger <rafaelberger.dev@gmail.com>
2022-06-26 17:39:59 -0300

    inscrição via GraphQl e página concluída

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4rwm3xh1pv501ywbfx35r1i/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
